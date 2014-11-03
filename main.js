1dabb1736672f8e3f7f4680104866cc6b13c879e
Christian Rasmussen <christian.rasmussen@bouvet.no>
2014-11-03 16:47:53 +0100

    Fixed reloading.

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-$(window).load(function() {
+$(function() {
   $.get("data.json", function (data) {
-    console.log(data);
-  });
+    var $flexslider = $(".flexslider");
+    var $slides = $flexslider.find(".slides");
 
-  $('.flexslider').flexslider({
-    controlNav: false,
-    directionNav: false,
+    var $pages = data.pages.map(function (page) {
+      return $("<li><iframe src='" + page + "'></iframe></li>");
+    });
 
-    animation: "slide",
-    animationLoop: true,
+    $pages.forEach(function ($page) {
+      $slides.append($page)
+    });
 
-    slideshowSpeed: 4000
+    var slideShowSpeed = data.pages.length * data.intervalPerPageMs;
+
+    function reloadPage($li) {
+      var $iframe = $li.find("iframe");
+      $iframe.attr("src", $iframe.attr("src"));
+    }
+
+    $flexslider.flexslider({
+      controlNav: false,
+      directionNav: false,
+
+      animation: "slide",
+      animationLoop: true,
+
+      slideshowSpeed: slideShowSpeed,
+
+      before: function (event) {
+        var nextSlideIndex = (event.currentSlide + 1) % event.count;
+        console.log("Reloading:" + data.pages[nextSlideIndex]);
+        reloadPage($pages[nextSlideIndex]);
+      }
+    });
   });
 });
