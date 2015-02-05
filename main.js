7586a2a86701d4444bc5ca4e16a8fb9debf6e552
Oddbjørn Haaland <oddbjorn.haaland@gmail.com>
2015-02-05 14:21:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $(function() {
       controlNav: false,
       directionNav: false,
 
-      animation: "fade",
+      animation: "slide",
       animationLoop: true,
 
       slideshowSpeed: slideShowSpeed,
